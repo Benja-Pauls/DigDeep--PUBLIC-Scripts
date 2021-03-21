d3a2600aeb476b198de87ad1426a38e72c6f86f0
Benja-Pauls <73416124+Benja-Pauls@users.noreply.github.com>
2021-03-21 01:12:47 -0500

    Update ButtonInteraction.lua

--- ButtonInteraction.lua
+++ ButtonInteraction.lua
@@ -6,31 +6,35 @@ wait(1)
 
 local Player = game.Players.LocalPlayer
 local Tycoons = workspace["Tycoon Game"]:WaitForChild("Tycoons") 
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ProximityPromptService = game:GetService("ProximityPromptService")
 
 local HumanoidRootPart = game.Workspace.Players:WaitForChild(tostring(Player)):WaitForChild("HumanoidRootPart")
 local Mouse = Player:GetMouse()
-local PurchaseObject = ReplicatedStorage.Events.Utility.PurchaseObject
-local LocalLoadTycoon = ReplicatedStorage.Events.Tycoon:WaitForChild("LocalLoadTycoon")
+local PurchaseObject = game.ReplicatedStorage.Events.Utility.PurchaseObject
+local LocalLoadTycoon = game.ReplicatedStorage.Events.Tycoon:WaitForChild("LocalLoadTycoon")
 local UIS = game:GetService("UserInputService")
 
-local Menu = script.Parent.Menu
-local ButtonGui = script:WaitForChild("ButtonGUI")
+local TycoonPurchaseMenu = script.Parent.TycoonPurchaseMenu
+local DisplayButtonGui = script:WaitForChild("DisplayButtonGUI")
+
+local ProximityPromptObjects = {}
 
 script.Parent.IgnoreGuiInset = true
-
-if LocalLoadTycoon then
-	print("Local Load Tycoon Event has been found")
-end
 
 LocalLoadTycoon.OnClientEvent:Connect(function(tycoon)
 	print("LocalLoadTycoon event has been fired")
 	local Buttons = tycoon.Buttons:GetChildren()
 	for i,button in pairs (Buttons) do
-		ButtonGui:Clone().Parent = button
+		local ProxPrompt = DisplayButtonGui:Clone()
+		ProxPrompt.Parent = button
+
+		ProxPrompt.AssociatedObject.Value = button
+		
+		ProxPrompt.VisibilityHandler.Disabled = false
+		--table.insert(ProximityPromptObjects, button)
 	end
 	
-	EInteract(tycoon)
+	--TycoonButtonInteract(tycoon)
  end)
 
 local function GetStatImage(File, Stat)
@@ -54,7 +58,7 @@ local function CheckUpgrade(OriginalObject, InfoList)
 end
 
 local function DisplayButtonMaterials(Button)
-	local CostList = Menu.CostsList
+	local CostList = TycoonPurchaseMenu.CostsList
 	local Slots
 	local OriginalSlot = game.ReplicatedStorage.GuiElements:FindFirstChild("TycoonPurchaseMaterialSlot")
 	
@@ -93,7 +97,7 @@ local function DisplayButtonMaterials(Button)
 end
 
 local function DisplayInformation(Button)
-	local InfoList = Menu.InformationList
+	local InfoList = TycoonPurchaseMenu.InformationList
 	local ReplicatedTycoonPurchases = game.ReplicatedStorage.TycoonPurchases
 	local ButtonType = Button:WaitForChild("Type").Value
 	local OriginalObject
@@ -103,8 +107,8 @@ local function DisplayInformation(Button)
 	else
 		OriginalObject = ReplicatedTycoonPurchases:FindFirstChild(tostring(Button.Object.Value))
 	end
-	Menu.Object.Text = tostring(Button.Object.Value)
-	Menu.Type.Text = tostring(ButtonType)
+	TycoonPurchaseMenu.Object.Text = tostring(Button.Object.Value)
+	TycoonPurchaseMenu.Type.Text = tostring(ButtonType)
 	
 	local UpgradeCheck = CheckUpgrade(OriginalObject, InfoList)
 	
@@ -122,8 +126,8 @@ local function DisplayInformation(Button)
 end
 
 local function DestroyButtonVisuals(Button)
-	local CostList = Menu.CostsList
-	local InfoList = Menu.InformationList
+	local CostList = TycoonPurchaseMenu.CostsList
+	local InfoList = TycoonPurchaseMenu.InformationList
 	
 	for i,visual in pairs(CostList:GetChildren()) do
 		visual:Destroy()
@@ -134,23 +138,23 @@ local function DestroyButtonVisuals(Button)
 	--end	
 end
 
-local press = false
+--[[
+--local press = false
 
-function EInteract(PlayerTycoon)
-	print("EInteract has been called")
+function TycoonButtonInteract(PlayerTycoon)
 	UIS.InputBegan:Connect(function(input)
 		if input.KeyCode == Enum.KeyCode.E then	
 			if Mouse.Target and (Mouse.Target.Position - HumanoidRootPart.Position).magnitude < 20 then
 				local Button = Mouse.Target
 				if Button:FindFirstChild("Price") ~= nil and Button.Parent == PlayerTycoon.Buttons then
-					if Menu.Visible == false and Button.Visible.Value == true then
+					if TycoonPurchaseMenu.Visible == false and Button.Visible.Value == true then
 						
 						DisplayButtonMaterials(Button)
 						DisplayInformation(Button)
 						
-						local purchaseButton = Menu.PurchaseButton
-						local exitButton = Menu.ExitButton
-						Menu.Visible = true
+						local purchaseButton = TycoonPurchaseMenu.PurchaseButton
+						local exitButton = TycoonPurchaseMenu.ExitButton
+						TycoonPurchaseMenu.Visible = true
 						
 						purchaseButton.Activated:Connect(function()
 							if press == false then
@@ -160,8 +164,8 @@ function EInteract(PlayerTycoon)
 								PurchaseObject:FireServer(Button)
 								wait(.1) --Wait For PurchaseHandler Money Check to Finish
 								
-								if Menu.CashWarning.Visible == false then
-									Menu.Visible = false
+								if TycoonPurchaseMenu.CashWarning.Visible == false then
+									TycoonPurchaseMenu.Visible = false
 									wait(1)
 									DestroyButtonVisuals(Button)
 									--Should set Mouse.Target to nil?
@@ -172,9 +176,9 @@ function EInteract(PlayerTycoon)
 						end)
 						
 						exitButton.Activated:Connect(function()
-							if press == false and Menu.Visible == true then 
+							if press == false and TycoonPurchaseMenu.Visible == true then 
 								press = true
-								Menu.Visible = false
+								TycoonPurchaseMenu.Visible = false
 								DestroyButtonVisuals(Button)
 								wait(.1)
 								press = false
@@ -189,24 +193,110 @@ function EInteract(PlayerTycoon)
 	local debounce = false
 	
 	--Mouse.Target.Changed:Connect(function(target)
-	
-	--Graphical (Again, is there a way to make this more efficient?)
+
 	while PlayerTycoon.Owner.Value == Player do --Way to make while loop more efficient?
 		wait(.1)
-		if Mouse.Target then
-			for i,v in pairs (PlayerTycoon.Buttons:GetChildren()) do
-				if Mouse.Target == v and (Mouse.Target.Position - HumanoidRootPart.Position).magnitude < 20 then 
-					if debounce == false and v.CanCollide == true then
-						debounce = true
-						v:WaitForChild("ButtonGUI").Enabled = true
-					end
-				else
-					if debounce == true and v:WaitForChild("ButtonGUI").Enabled == true then
-						v:WaitForChild("ButtonGUI").Enabled = false
-						debounce = false
+		Mouse.Target.Changed:Connect(function(target)
+			print("Mouse.Target has changed to: ",target)
+			if target then
+				for i,v in pairs (PlayerTycoon.Buttons:GetChildren()) do
+					if target == v and (target.Position - HumanoidRootPart.Position).magnitude < 20 then 
+						if debounce == false and v.CanCollide == true then
+							debounce = true
+							v:WaitForChild("DisplayButtonGUI").Enabled = true
+						end
+					else
+						if debounce == true and v:WaitForChild("DisplayButtonGUI").Enabled == true then
+							v:WaitForChild("DisplayButtonGUI").Enabled = false
+							debounce = false
+						end
 					end
 				end
 			end
-		end
+		end)
 	end
 end
+]]
+
+
+function HotkeyInteraction(promptObject, player)
+	print(player, promptObject)
+	
+	local AssociatedObject = promptObject.AssociatedObject.Value
+	
+	
+	
+	--check if a tycoon button and check if player == Owner, therefore, they can interact with it
+	
+	--https://developer.roblox.com/en-us/articles/proximity-prompts
+	--Proximity Prompts basically do all of the work, so all that has to be recognized for each object is what happens
+	--in an easy and concise way that can be handled by one or a few functions
+	
+	
+end
+
+--[[
+local function UpdateProxPromptVisibility(ObjectVisibility, AssociatedProxPrompt, number)
+	if number then
+		if ObjectVisibility >= 1 then
+			AssociatedProxPrompt.Enabled = false
+		else
+			AssociatedProxPrompt.Enabled = true
+		end
+	else
+		AssociatedProxPrompt.Enabled = ObjectVisibility
+	end
+end
+
+local function ManageProxPromptVisibility(Object, AssociatedProxPrompt)
+	
+	if Object:IsA("Model") then
+		local ObjectVisibility = Object:FindFirstChild("Visible").Value
+		UpdateProxPromptVisibility(ObjectVisibility, AssociatedProxPrompt)
+		
+		Object:FindFirstChild("Visible").Changed:Connect(function()
+			UpdateProxPromptVisibility(ObjectVisibility, AssociatedProxPrompt)
+		end)
+	else
+		local ObjectVisibility = Object.Transparency
+		UpdateProxPromptVisibility(ObjectVisibility, AssociatedProxPrompt, true)
+		
+		Object:GetPropertyChangedSignal("Transparency"):Connect(function()
+			UpdateProxPromptVisibility(ObjectVisibility, AssociatedProxPrompt, true)
+		end)
+	end
+end
+
+local function SetUpProximityPrompts()
+	for i,proxPrompt in pairs (game.Workspace:GetDescendants()) do
+		if proxPrompt:IsA("ProximityPrompt") then
+			local Object = proxPrompt.AssociatedObject.Value
+			table.insert(ProximityPromptObjects, Object)
+			table.insert(ProximityPromptObjects, proxPrompt)
+		end
+	end 
+	
+	for i=1,#ProximityPromptObjects,1 do
+		if not ProximityPromptObjects[i]:IsA("ProximityPrompt") then
+			ManageProxPromptVisibility(ProximityPromptObjects[i], ProximityPromptObjects[i+1])
+		end
+	end
+	
+	--how to grab new values added to table
+	ProximityPromptObjects.Changed:Connect(function(newObject)
+		print(newObject)
+	end)
+	
+end
+
+--module script will manage the table values with the server, and the HotKeyInteractionHandler will take care of 
+--what interacting with the ProximityPrompt does
+
+SetUpProximityPrompts()
+]]
+
+
+ProximityPromptService.PromptTriggered:Connect(HotkeyInteraction)
+
+--make a table of object values of all objects in workspace with proximity prompt and check their visiblilities for
+--their guaranteed proximity prompt's enabled value
