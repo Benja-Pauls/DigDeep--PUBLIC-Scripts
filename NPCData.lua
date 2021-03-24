f51c14cd6c8deea75576de5246d93a3117ddd849
Benja-Pauls <73416124+Benja-Pauls@users.noreply.github.com>
2021-03-23 23:16:58 -0500

    Update NPCData.lua

--- NPCData.lua
+++ NPCData.lua
@@ -21,19 +21,20 @@ local NPCData = {
 		},
 		
 		["Basic Dialogue"] = { --Does [index] need to be present?
-			["Dialogue1"] = {[1]="Hello, I am Test Man!", [2]="Happy"},
-			["Dialogue2"] = {[1]="Hi There Stranger!", [2]="Neutral"},
-			["Dialogue3"] = {[1]="Goodbye!", [2]="Neutral"}
+			["Dialogue1"] = {"Hello, I am Test Man!", "Happy"},
+			["Dialogue2"] = {"Hi There Stranger!", "Neutral"},
+			["Dialogue3"] = {"Goodbye!", "Neutral"}
 			--Does Lua use Table[-1]? Could have final index be goodbye statement string
 		},
 		
 		["Conversations"] = {
-			["Dialogue1"] = {[1]="Hi There", [2]="I'm Test Man"}
+			["Dialogue1"] = {"Hi There", "I'm Test Man"}
 		}, 
 		
-		["Items"] = {
-			RS.Equippable.Bags.OreBags["Advanced Bag"],
-			RS.Equippable.Bags.PlantBags["Advanced Bag"]
+		["Items"] = { --Sort by type
+			{RS.Equippable.Bags.OreBags["Advanced Bag"], 10},
+			{RS.Equippable.Bags.PlantBags["Advanced Bag"], 10},
+			{RS.Equippable.Tools.Pickaxes.Pickaxe, 5}
 			
 		}	
 	}	
@@ -42,9 +43,6 @@ local NPCData = {
 	
 	
 }
-
-
-
 
 --grab items directly from replicated storage so shop item inserter does not have the find them, it will only have
 --to go through each element of this table to get item info to be displayed
