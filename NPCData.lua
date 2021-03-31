81ea900d26a71267c8185348b7297eb77353fa76
Benja-Pauls <73416124+Benja-Pauls@users.noreply.github.com>
2021-03-31 01:10:24 -0500

    Update NPCData.lua

--- NPCData.lua
+++ NPCData.lua
@@ -31,11 +31,12 @@ local NPCData = {
 		
 		["Dialogue"] = { --Does [index] need to be present?
 			["Dialogue1"] = {"Hello, I am Test Man!", "Happy"},
-			["Dialogue2"] = {"Hi There Stranger!", "Neutral"},
+			["Dialogue2"] = {"Hi There PLAYER!", "Neutral"},
 			["Dialogue3"] = {"How can I help you?", "Neutral"},
-			["Goodbye1"] = {"Goodbye!", "Neutral"},
-			["Cannot Afford Item"] = {"......."},
-			["Item Already Purchased"] = {"......."}, 
+			["Goodbye"] = {"Goodbye!", "Neutral"},
+			["Cannot Afford Item"] = {"Sorry, you need MISSINGFUNDS more to purchase the ITEM"},
+			["Item Already Purchased"] = {"You already have the ITEM!", "Neutral"},
+			["Thank You For Purchase"] = {"Thank you for your purchase!", "Happy"}
 			
 			
 			--Does Lua use Table[-1]? Could have final index be goodbye statement string
@@ -50,7 +51,8 @@ local NPCData = {
 			{RS.Equippable.Bags.OreBags["Advanced Ore Bag"], 10},
 			{RS.Equippable.Bags.PlantBags["Beginner Plant Bag"], 4},
 			{RS.Equippable.Bags.PlantBags["Advanced Plant Bag"], 10},
-			{RS.Equippable.Tools.Pickaxes.Pickaxe, 5}
+			{RS.Equippable.Tools.Pickaxes.Pickaxe, 5},
+			{RS.Equippable.Tools.Pickaxes["Orange Pickaxe"], 15}
 			
 		}	
 	}	
