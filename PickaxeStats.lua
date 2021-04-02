8919a1a59e95831c5f463e536d65c34855154a81
Benja-Pauls <73416124+Benja-Pauls@users.noreply.github.com>
2021-04-02 00:07:00 -0500

    Update PickaxeStats.lua

--- PickaxeStats.lua
+++ PickaxeStats.lua
@@ -1,23 +1,21 @@
 --(ModuleScript)
 --ModuleScript that every level of pickaxe will have to easily access it's information
 -----------------------------------------------------------------------------------------------------------------------------------------------
---This is similar to creating a class in Python, we're defining the "property" of Eff for the 
---"object" Pickaxe
 
 local Pickaxe = {
 	
 	["Stats"] = {
-		{"PickaxeEfficiency", 6},
-		{"PickaxeDelay", 0.5},
-		{"PickaxeReach", 4},
+		{"PickaxesEfficiency", 6},
+		{"PickaxesDelay", 0.5},
+		{"PickaxesReach", 4},
 		{"TestStat", true} --Test ImageName index check to fill Tool Slot statistic bars
 			
 	},
 	
 	["Images"] = { --ImageId, Type = (Where it will be placed) (no index 2 means no display of stat in tile or ItemViewer)
-		["PickaxeEfficiencyImage"] = {"rbxassetid://6471867379", "StatBar"},
-		["PickaxeDelayImage"] = {"rbxassetid://6471867379", "StatBar"},
-		["PickaxeReachImage"] ={"rbxassetid://6471867379", "StatBar"},
+		["PickaxesEfficiencyImage"] = {"rbxassetid://6471867379", "StatBar"},
+		["PickaxesDelayImage"] = {"rbxassetid://6471867379", "StatBar"},
+		["PickaxesReachImage"] ={"rbxassetid://6471867379", "StatBar"},
 		--["TestStatImage"] = {"TestStatImageId", "Badge"}
 		
 	}
@@ -27,3 +25,4 @@ local Pickaxe = {
 
 
 return Pickaxe
+
