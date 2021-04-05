39dc0cd1dc4f5bff872a91476286a3ee505524c1
Benja-Pauls <73416124+Benja-Pauls@users.noreply.github.com>
2021-04-04 21:40:36 -0500

    Update TycoonAssetsHandler.lua

--- TycoonAssetsHandler.lua
+++ TycoonAssetsHandler.lua
@@ -3,196 +3,221 @@
 ------------------------------------------------------------------------------------------------------------------------------------------------
 local Objects = {}
 
-repeat wait() until script.Parent:FindFirstChild("Buttons")
+repeat wait() until script.Parent:FindFirstChild("Buttons") --Script has been moved to tycoon
 
 local Tycoon = script.Parent
-local TeamColor = Tycoon:WaitForChild("TeamColor").Value
 local CollectibleCash = Tycoon.CurrencyToCollect
-local Debris = game:GetService('Debris')
+local PurchasedObjects = Tycoon.PurchasedObjects
+local FinishedResearch = Tycoon.FinishedResearch
+
+local PlayerDataFolder = game.ServerStorage:FindFirstChild("PlayerData")
+
 local SoundEffects = require(game.ReplicatedStorage:WaitForChild("SoundEffects"))
-local PurchaseObject = game:GetService("ReplicatedStorage").Events.Utility.PurchaseObject
 local PlayerStatManager = require(game.ServerScriptService:FindFirstChild("PlayerStatManager"))
-local PlayerData = game.ServerStorage:FindFirstChild("PlayerData")
 local Utility = require(game.ServerScriptService:WaitForChild("Utility"))
+
+local PurchaseObject = game:GetService("ReplicatedStorage").Events.Utility.PurchaseObject
 local UpdateInventory = game.ReplicatedStorage.Events.GUI:FindFirstChild("UpdateInventory")
 
-Tycoon.Essentials.Spawn.TeamColor = TeamColor
-Tycoon.Essentials.Spawn.BrickColor = TeamColor
+--local TeamColor = Tycoon:WaitForChild("TeamColor").Value
+--Tycoon.Essentials.Spawn.TeamColor = TeamColor
+--Tycoon.Essentials.Spawn.BrickColor = TeamColor
 
-local function CheckDependencies(Dependencies)
-	local CompletedDependencies = 0
-	local PurchasedObjects = Tycoon.PurchasedObjects
-	--local DependencyAmount = Dependencies:GetChildren()
-	for i,v in pairs (Dependencies:GetChildren()) do
-		if PurchasedObjects:FindFirstChild(tostring(v.Value)) then
-			CompletedDependencies = CompletedDependencies + 1
-		end
-	end
-
-	return CompletedDependencies
-end
-
---DropCollector Processing, Checks Current Terminal Amount Cap
-local HandleDropMaterialsEvent = game.ReplicatedStorage.Events.Tycoon:WaitForChild("HandleDropMaterials")
-for i,v in pairs(Tycoon.Essentials:GetChildren()) do 
-	if v.Name == "DropCollector" then 
-		v.Touched:connect(function(Part)
-			if Part:FindFirstChild("Cash") then
-				HandleDropMaterialsEvent:Fire(Tycoon, Part)
-				local TerminalLevels = Tycoon.Essentials.TerminalLevel:GetChildren()
-				if #TerminalLevels == 1 then --Can make this more efficient, value check for <=
-					if Part.Cash.Value + CollectibleCash.Value <= 100 then
-						CollectibleCash.Value = CollectibleCash.Value + Part.Cash.Value
-						Debris:AddItem(Part,0.1)
-					else
-						Debris:AddItem(Part,0.1)
-					end
-				end
-				if #TerminalLevels == 2 then
-					if Part.Cash.Value + CollectibleCash.Value <= 200 then
-						CollectibleCash.Value = CollectibleCash.Value + Part.Cash.Value
-						Debris:AddItem(Part,0.1)
-					else
-						Debris:AddItem(Part,0.1)
-					end
-				end
-			end
-		end)
-	end
-end
-
---Get Money From Collector
-coroutine.resume(coroutine.create(function()
-	local Givers = Tycoon.Essentials:WaitForChild("Givers")
-	if Givers:WaitForChild("Giver") ~= nil then
-		local loop = true
-		while loop == true do --checks for amount of givers, way to make more efficient?
-			wait(1)
-
-			for i,v in pairs(Givers:GetChildren()) do
-				debounce = false
-				
-				v.Touched:connect(function(hit)
-					local player = game.Players:GetPlayerFromCharacter(hit.Parent)
-					
-					if player ~= nil then
-						if Tycoon.Owner.Value == player then
-							if hit.Parent:FindFirstChild("Humanoid") then
-								if hit.Parent.Humanoid.Health > 0 then
-									if debounce == false then
-										debounce = true 
-										v.BrickColor = BrickColor.new("Bright red")
-										
-										local PlayerDataFile = PlayerData:FindFirstChild(tostring(player.UserId))
-										local PlayerCash = PlayerDataFile:FindFirstChild("Currencies"):FindFirstChild("UniversalCurrencies"):FindFirstChild("Currency")
-										if PlayerCash ~= nil then 
-											local ListedPlayer = game.Players:FindFirstChild(tostring(player))
-											if ListedPlayer ~= nil then
-												SoundEffects:PlaySound(v, SoundEffects.Tycoon.Collect) 
-												
-												--UPDATE PLAYER CASH VIA PLAYER STAT MANAGER
-												--COLLECTIBLE CASH IS SAVED IN PLAYERDATA FOR THEIR TYCOON (OWNSTYCOON VALUE)
-												--AND YOU COMPARE THAT VALUE (CHECK IF WITHIN SIGNIFICANCE LEVEL OF .05)
-												
-												PlayerCash.Value = PlayerCash.Value + CollectibleCash.Value --add dropper cash to player cash
-
-												Utility:UpdateMoneyDisplay(hit.Parent.Name, PlayerCash.Value)
-												if CollectibleCash.Value ~= 0 then
-													UpdateInventory:FireClient(player, "Currency", "Currencies", nil, CollectibleCash.Value, "Inventory", "Money1")
-												end	
-
-												CollectibleCash.Value = 0 
-												wait(1)
-												v.BrickColor = BrickColor.new("Sea green")
-												debounce = false 
-											else
-												print("Breaking Giver Loop for " .. tostring(player))
-												loop = false
-											end
-										end
-									end
-								end
-							end
-						end
-					end
-				end)
-			end
-		end	
-	end
-end))
-
-local function PrepButton(Button, Parts)
-	Button:WaitForChild("Visible").Value = true
-	for bp = 1,#Parts,1 do
-		Parts[bp].Transparency = 0
-		Button.CanCollide = true
-	end
-end
+-------------------<|Utility|>---------------------------------------------------------------------------------------------------------------------------
 
 local function GetPlayer(WantedPlayer)
 	print("WANTED PLAYER: " .. tostring(WantedPlayer))
 	local Players = game.Players:GetChildren()
 	local lookingForPlayer = true
-	for i,v in pairs(Players) do
+	for i,v in pairs (Players) do
 		if tostring(v.UserId) == WantedPlayer then
 			lookingForPlayer = false
 			return v
 		end
 	end
 	if lookingForPlayer == true then
-		warn("Something went wrong, cannot find player")
+		warn("Something went wrong, cannot find " .. tostring(WantedPlayer))
 	end
 end
 
---Prepping Tycoon Purchases
-for i,button in pairs(Tycoon.Buttons:GetChildren()) do
+----------------<|Collector Functions|>------------------------------------------------------------------------------------------------------------------
 
-	coroutine.resume(coroutine.create(function()
+local Debris = game:GetService("Debris")
+local HandleDropMaterialsEvent = game.ReplicatedStorage.Events.Tycoon:WaitForChild("HandleDropMaterials")
+local function PrepDropCollectors(collector)
+	collector.Touched:connect(function(Part)
+		if Part:FindFirstChild("Cash") then
+			HandleDropMaterialsEvent:Fire(Tycoon, Part)
 
-		local ButtonParts = button.Model:GetChildren()
-		if button:FindFirstChild("Object") then 
-			local TycoonPurchases = game.ReplicatedStorage:FindFirstChild("TycoonPurchases")
-			local ButtonType = button:WaitForChild("Type").Value --Used for file management in TycoonPurchases file
-			local Object
-			if TycoonPurchases:FindFirstChild(tostring(ButtonType)) then
-				local TypeFile = TycoonPurchases:FindFirstChild(tostring(ButtonType))
-				Object = TypeFile:FindFirstChild(tostring(button.Object.Value))
-			else
-				Object = TycoonPurchases:FindFirstChild(button.Object.Value)
-			end
-			if Object ~= nil then --If object affiliated is in the purchases group
-				Objects[Object.Name] = Object:Clone()
-				--Object:Destroy() --All objects are stored in a table, not bought = destroyed until bought
-				--print(tostring(Object).." was destroyed")
-			else
-				error("Button "..tostring(button).." doesn't have a valid object!")
-			end
+			CollectibleCash.Value = CollectibleCash.Value + Part.Cash.Value
+			Debris:AddItem(Part,0.1)
+		end
+	end)
+end
 
-			if button:FindFirstChild("Dependencies") then
-				button.CanCollide = false
-				button:WaitForChild("Visible").Value = false
-				for bp = 1,#ButtonParts,1 do
-					ButtonParts[bp].Transparency = 1
-				end
-				coroutine.resume(coroutine.create(function()
-					local repeating = true
-					while repeating do
-						wait(3)
-						local Dependencies = button.Dependencies
-						local DependencyAmount = button.Dependencies:GetChildren()
+for i,collector in pairs (Tycoon.Essentials:GetChildren()) do
+	if collector.Name == "DropCollector" then 
+		PrepDropCollectors(collector)
+	end
+end
+Tycoon.Essentials.ChildAdded:Connect(function(newCollector)
+	if newCollector.Name == "DropCollector" then
+		PrepDropCollectors(newCollector)
+	end
+end)
 
-						local FulfilledDependencies = CheckDependencies(Dependencies)
-						if FulfilledDependencies == #DependencyAmount then
-							repeating = false
-							PrepButton(button,ButtonParts)
+---------------------<|Giver Functions|>--------------------------------------------------------------------------------------------------------------------
+
+local function PrepGiverButton(giver)
+	debounce = false
+	giver.Touched:connect(function(hit)
+		local player = game.Players:GetPlayerFromCharacter(hit.Parent)
+
+		if player ~= nil then
+			if Tycoon.Owner.Value == player then
+				if hit.Parent:FindFirstChild("Humanoid") then
+					if hit.Parent.Humanoid.Health > 0 then
+						if debounce == false then
+							debounce = true 
+							giver.BrickColor = BrickColor.new("Bright red")
+
+							local PlayerDataFile = PlayerDataFolder:FindFirstChild(tostring(player.UserId))
+							local PlayerCash = PlayerDataFile:FindFirstChild("Currencies"):FindFirstChild("UniversalCurrencies"):FindFirstChild("Currency")
+							if PlayerCash ~= nil then 
+								local ListedPlayer = game.Players:FindFirstChild(tostring(player))
+								if ListedPlayer ~= nil then
+									SoundEffects:PlaySound(giver, SoundEffects.Tycoon.Collect) 
+
+									PlayerCash.Value = PlayerCash.Value + CollectibleCash.Value --add dropper cash to player cash
+
+									Utility:UpdateMoneyDisplay(hit.Parent.Name, PlayerCash.Value)
+									if CollectibleCash.Value ~= 0 then
+										UpdateInventory:FireClient(player, "Currency", "Currencies", nil, CollectibleCash.Value, "Inventory", "Money1")
+									end	
+
+									CollectibleCash.Value = 0 
+									wait(1)
+									giver.BrickColor = BrickColor.new("Sea green")
+									debounce = false 
+								else
+									print("Breaking Giver Loop for " .. tostring(player))
+								end
+							end
 						end
 					end
-				end))
-			else
-				button:WaitForChild("Visible").Value = true --WaitForChild is there for now, not all buttons have it yet
+				end
 			end
 		end
-	end))
+	end)
+end
+
+local Givers = Tycoon.Essentials:WaitForChild("Givers")
+
+for i,giver in pairs (Givers:GetChildren()) do
+	PrepGiverButton(giver)
+end
+Givers.ChildAdded:Connect(function(newGiver)
+	PrepGiverButton(newGiver)
+end)
+
+-----------------------<|Tycoon Button Functions|>--------------------------------------------------------------------------------------------------------
+
+local function UpdateButtonVisibility(Button, Parts, Bool, Transparency)
+	Button:WaitForChild("Visible").Value = Bool
+	for bp = 1,#Parts,1 do
+		Parts[bp].Transparency = Transparency
+		Button.CanCollide = Bool
+	end
+end
+
+local function SeeIfObjectIsDependency(Dependencies, Object)
+	for i,dependency in pairs (Dependencies) do
+		if dependency.Value == tostring(Object) then
+			return true
+		end
+	end
+end
+
+local function CountDependencies(Dependencies, Folder)
+	local CompletedDependencies = 0
+	for i,v in pairs (Dependencies) do
+		if Folder:FindFirstChild(tostring(v.Value)) then
+			CompletedDependencies += 1
+		end
+	end
+
+	return CompletedDependencies
+end
+
+local function ReviewDependencies(Button, ButtonParts)
+	local Dependencies = Button.Dependencies
+	local DependencyTypesToMeet = #Dependencies:GetChildren()
+	local DependenciesMet = 0
+	
+	for i,dependency in pairs (Dependencies:GetChildren()) do
+		if dependency.Value == true then
+			DependenciesMet += 1
+		end
+	end
+	
+	if DependencyTypesToMeet == DependenciesMet then
+		UpdateButtonVisibility(Button, ButtonParts, true, 0)
+	end
+end
+
+local function SetUpDependencyChecks(Button, ButtonParts, CompletedOfType, DependencyType, Object)
+	local Dependencies = Button.Dependencies
+	local TypeDependencies = DependencyType:GetChildren()
+
+	CompletedOfType.ChildAdded:Connect(function(newObject)
+		if not CompletedOfType:FindFirstChild(tostring(Object)) then
+			if SeeIfObjectIsDependency(TypeDependencies, newObject) then
+				local FulfilledDependencies = CountDependencies(TypeDependencies, CompletedOfType)
+
+				if FulfilledDependencies == #TypeDependencies then
+					DependencyType.Value = true
+					ReviewDependencies(Button, ButtonParts)
+				end
+			end
+		end
+	end)
+end
+
+for i,button in pairs(Tycoon.Buttons:GetChildren()) do
+	if button:FindFirstChild("Object") then 
+		local TycoonPurchases = game.ReplicatedStorage:FindFirstChild("TycoonPurchases")
+		local ButtonType = button:WaitForChild("Type").Value --Used for file management in TycoonPurchases file
+		local Object
+		
+		if TycoonPurchases:FindFirstChild(tostring(ButtonType)) then
+			local TypeFile = TycoonPurchases:FindFirstChild(tostring(ButtonType))
+			Object = TypeFile:FindFirstChild(tostring(button.Object.Value))
+		else
+			Object = TycoonPurchases:FindFirstChild(button.Object.Value)
+		end
+		if Object then --If object affiliated is in the purchases group
+			Objects[Object.Name] = Object:Clone()
+		else
+			error("Button "..tostring(button).." doesn't have a valid object!")
+		end
+		
+		local ButtonParts = button.ButtonModel:GetChildren()
+		if button:FindFirstChild("Dependencies") then
+			local Dependencies = button.Dependencies
+			UpdateButtonVisibility(button, ButtonParts, false, 1)
+			
+			if Dependencies:FindFirstChild("TycoonObjects") then
+				local TycoonObjects = Dependencies.TycoonObjects
+				SetUpDependencyChecks(button, ButtonParts, PurchasedObjects, TycoonObjects, Object)
+			end
+			if Dependencies:FindFirstChild("TycoonResearch") then
+				local TycoonResearch = Dependencies.TycoonResearch
+				SetUpDependencyChecks(button, ButtonParts, FinishedResearch, TycoonResearch, Object)
+			end
+		else
+			UpdateButtonVisibility(button, ButtonParts, true, 0)
+		end
+	end
 end
 
 return Objects
