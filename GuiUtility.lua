1ebcfdaf49cbe9d799c9b90945e938b5339429ee
Benja-Pauls <73416124+Benja-Pauls@users.noreply.github.com>
2021-06-17 23:45:56 -0500

    Create GuiUtility.lua

---
+++ GuiUtility.lua
@@ -0,0 +1,132 @@
+local GuiUtility = {}
+
+--------------<|Text Effects|>-------------------------------------------------------------
+
+function GuiUtility.typeWrite(guiObject, text, delayBetweenChars)
+	guiObject.Visible = true
+	guiObject.AutoLocalize = false
+
+	local displayText = text
+	guiObject.Text = displayText
+
+	local index = 0
+	for first, last in utf8.graphemes(displayText) do
+		index = index + 1
+		guiObject.MaxVisibleGraphemes = index
+		wait(delayBetweenChars)
+	end
+end
+
+function GuiUtility.ConvertShort(Filter_Num) --this function is also in PlayerStatManager for server
+	local x = tostring(Filter_Num)
+	
+	if #x>=10 then
+		local important = (#x-9)
+		return x:sub(0,(important)).."."..(x:sub(#x-7,(#x-7))).."B"
+	elseif #x>=7 then
+		local important = (#x-6)
+		return x:sub(0,(important)).."."..(x:sub(#x-5,(#x-5))).."M"
+	elseif #x>=4 then
+		local important = (#x-3)
+		return x:sub(0,(important)).."."..(x:sub(#x-2,(#x-2))).."K"
+	else
+		return Filter_Num
+	end
+end
+
+-------------<|Menu Display Functions|>------------------------------------------------
+
+function GuiUtility.Display3DModels(Player, viewport, displayModel, bool, displayAngle)
+	--possibly clear all viewports once menu is closed? (or once viewport is not visible?)
+	print("Display3DModels: ", Player, viewport, displayModel, bool, displayAngle)
+	if bool == true then
+		GuiUtility.Display3DModels(Player, viewport, displayModel:Clone(), false) --reset current viewPort
+
+		local rootPart
+		if displayModel.Name == tostring(Player) then
+			displayModel.Parent = viewport.Physics
+			rootPart = displayModel.HumanoidRootPart
+
+			local idleAnimation = displayModel.Humanoid:LoadAnimation(displayModel.Animate.idle.Animation1)
+			idleAnimation:Play()
+		else
+			local ParentModel = Instance.new("Model", viewport.Physics)
+			displayModel.Parent = ParentModel
+			rootPart = displayModel
+			displayModel = ParentModel
+		end
+		local vpCamera = Instance.new("Camera",viewport)
+
+		rootPart.Name = "RootPart"
+		rootPart.Anchored = true
+		viewport.CurrentCamera = vpCamera
+
+		--Move Camera Around Object & Auto FOV
+		local referenceAngle = Instance.new("NumberValue", rootPart)
+		referenceAngle.Name = "ReferenceAngle"
+		referenceAngle.Value = displayAngle
+		local modelCenter, modelSize = displayModel:GetBoundingBox()	
+
+		local rotInv = (modelCenter - modelCenter.p):inverse()
+		modelCenter = modelCenter * rotInv
+		modelSize = rotInv * modelSize
+		modelSize = Vector3.new(math.abs(modelSize.x), math.abs(modelSize.y), math.abs(modelSize.z))
+
+		local diagonal = 0
+		local maxExtent = math.max(modelSize.x, modelSize.y, modelSize.z)
+		local tan = math.tan(math.rad(vpCamera.FieldOfView/2))
+
+		if (maxExtent == modelSize.x) then
+			diagonal = math.sqrt(modelSize.y*modelSize.y + modelSize.z*modelSize.z)/2
+		elseif (maxExtent == modelSize.y) then
+			diagonal = math.sqrt(modelSize.x*modelSize.x + modelSize.z*modelSize.z)/2
+		else
+			diagonal = math.sqrt(modelSize.x*modelSize.x + modelSize.y*modelSize.y)/2
+		end
+
+		local minDist = (maxExtent/4)/tan + diagonal
+		game:GetService("RunService").RenderStepped:Connect(function(dt)
+			referenceAngle.Value += (1*dt*60)/3
+			vpCamera.CFrame = modelCenter * CFrame.fromEulerAnglesYXZ(0, math.rad(referenceAngle.Value), 0) * CFrame.new(0, 0, minDist + 3)
+		end)
+	else
+		viewport.CurrentCamera = nil
+		for i,view in pairs (viewport.Physics:GetChildren()) do
+			view:Destroy()
+		end
+	end
+end
+
+function GuiUtility.Reset3DObject(Player, viewport, displayModel, angle)
+	viewport.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1) then
+			if displayModel == nil then
+				local differentDisplayModel = viewport.Physics.Model.RootPart
+				local differentAngle = viewport.Physics.Model.RootPart.ReferenceAngle.Value
+				if differentDisplayModel ~= nil then
+					GuiUtility.Display3DModels(Player, viewport, differentDisplayModel:Clone(), true, differentAngle)
+				end
+			else
+				GuiUtility.Display3DModels(Player, viewport, displayModel:Clone(), true, angle)
+			end
+		end
+	end)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return GuiUtility
+
