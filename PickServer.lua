a4c6d5b30213edf2198e1f8c6a4bb7ad798f9eed
Benja-Pauls <73416124+Benja-Pauls@users.noreply.github.com>
2021-07-01 02:00:30 -0500

    Update PickServer.lua

--- PickServer.lua
+++ PickServer.lua
@@ -63,12 +63,12 @@ local function StartMining()
 						MouseDown = true
 						Tool.IsMining.Value = true
 
-						local TimeToMine = (ItemInfo.Strength.Value / FindStatValue(ToolStats["Stats"], "PickaxesEfficiency"))
+						local TimeToMine = (ItemInfo.Strength.Value / FindStatValue(ToolStats["Stats"], "Dig Power"))
 						local WaitTime = 0
 						
 						Debounce = false --Prevents mining to happen again until this block has been mined
 						coroutine.resume(coroutine.create(function()
-							wait(TimeToMine + FindStatValue(ToolStats["Stats"], "PickaxesDelay"))
+							wait(TimeToMine + FindStatValue(ToolStats["Stats"], "Speed"))
 							Debounce = true
 						end))
 						
@@ -88,7 +88,7 @@ local function StartMining()
 							Target.Reflectance = 0
 						end
 
-						wait(FindStatValue(ToolStats["Stats"], "PickaxesDelay"))
+						wait(FindStatValue(ToolStats["Stats"], "Speed"))
 						Debounce = true
 						if MouseDown then --keep mining if mouse is still down
 							StartMining()
