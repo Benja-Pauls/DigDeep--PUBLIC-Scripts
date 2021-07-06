f7165ecd737384c43c2ea14b5efafd6f5756897b
Benja-Pauls <73416124+Benja-Pauls@users.noreply.github.com>
2021-07-06 00:10:54 -0500

    Update PickServer.lua

--- PickServer.lua
+++ PickServer.lua
@@ -50,62 +50,66 @@ local function StartMining()
 
 			local itemName
 			if Target.Name == "Target" then --Model represents object, not Part
-				itemName = Target.Parent.Name
+				if Target.Parent then
+					itemName = Target.Parent.Name
+				end
 			else
 				itemName = Target.Name
 			end
 			
-			local ItemInfo = FindItemInfo(itemName)
-			if ItemInfo then
-				local ItemCount,BagCapacity,BagType = GetBagCount:Invoke(Player, ItemInfo)
-				
-				if ItemCount and BagCapacity then
-					if ItemCount < BagCapacity then --If Bag is not full
-						MouseDown = true
-						Tool.IsMining.Value = true
+			if itemName then
+				local ItemInfo = FindItemInfo(itemName)
+				if ItemInfo then
+					local ItemCount,BagCapacity,BagType = GetBagCount:Invoke(Player, ItemInfo)
+					
+					if ItemCount and BagCapacity then
+						if ItemCount < BagCapacity then --If Bag is not full
+							MouseDown = true
+							Tool.IsMining.Value = true
 
-						local TimeToMine = (ItemInfo.Strength.Value / FindStatValue(ToolStats["Stats"], "Dig Power"))
-						local WaitTime = 0
-						
-						Debounce = false --Prevents mining to happen again until this block has been mined
-						coroutine.resume(coroutine.create(function()
-							wait(TimeToMine + FindStatValue(ToolStats["Stats"], "Mining Speed"))
-							Debounce = true
-						end))
-						
-						repeat
-							wait(0.01)
-							WaitTime = WaitTime + 0.01
+							local TimeToMine = (ItemInfo.Strength.Value / FindStatValue(ToolStats["Stats"], "Dig Power"))
+							local WaitTime = 0
 							
-							Target.Reflectance = WaitTime/TimeToMine --use target's reflectance as reference across scripts
-							--print("Progress: " .. tostring(Target.Reflectance))
-						until WaitTime >= TimeToMine or not MouseDown or Target ~= Tool.Target.Value
-						
-						local mined = true
-						if WaitTime >= TimeToMine and mined then
-							MineOre:Fire(Player, Target)
-							mined = false
-						else
-							Target.Reflectance = 0
-						end
+							Debounce = false --Prevents mining to happen again until this block has been mined
+							coroutine.resume(coroutine.create(function()
+								wait(TimeToMine + FindStatValue(ToolStats["Stats"], "Mining Speed"))
+								Debounce = true
+							end))
+							
+							repeat
+								wait(0.01)
+								WaitTime = WaitTime + 0.01
+								
+								Target.Reflectance = WaitTime/TimeToMine --use target's reflectance as reference across scripts
+								--print("Progress: " .. tostring(Target.Reflectance))
+							until WaitTime >= TimeToMine or not MouseDown or Target ~= Tool.Target.Value
+							
+							local mined = true
+							if WaitTime >= TimeToMine and mined then
+								MineOre:Fire(Player, Target)
+								mined = false
+							else
+								Target.Reflectance = 0
+							end
 
-						wait(FindStatValue(ToolStats["Stats"], "Mining Speed"))
-						Debounce = true
-						if MouseDown then --keep mining if mouse is still down
-							StartMining()
+							wait(FindStatValue(ToolStats["Stats"], "Mining Speed"))
+							Debounce = true
+							if MouseDown then --keep mining if mouse is still down
+								StartMining()
+							else
+								Tool.IsMining.Value = false
+							end
 						else
-							Tool.IsMining.Value = false
+							UpdateItemCount:FireClient(Player, ItemCount, BagCapacity, BagType)
 						end
 					else
-						UpdateItemCount:FireClient(Player, ItemCount, BagCapacity, BagType)
+						print("Player does not have a bag equipped!", Player, ItemCount, 0, BagType)
+						UpdateItemCount:FireClient(Player, ItemCount, 0, BagType)
 					end
-				else
-					print("Player does not have a bag equipped!", Player, ItemCount, 0, BagType)
-					UpdateItemCount:FireClient(Player, ItemCount, 0, BagType)
 				end
+			else
+				Tool.IsMining.Value = false
 			end
-		else
-			Tool.IsMining.Value = false
 		end
 	end
 end
