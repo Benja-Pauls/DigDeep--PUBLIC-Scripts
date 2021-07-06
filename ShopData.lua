312e58a8d853d4c044cd921d5694b6f6d3d8cd0b
Benja-Pauls <73416124+Benja-Pauls@users.noreply.github.com>
2021-07-06 00:12:31 -0500

    Update ShopData.lua

--- ShopData.lua
+++ ShopData.lua
@@ -49,9 +49,54 @@ local NPCData = {
 			{RS.Equippable.Tools.Pickaxes["Orange Pickaxe"], 15, "Coins"},
 			{RS.Equippable.Tools.Pickaxes["Glow Pickaxe"], 1200, "Coins"}
 		}	
-	}	
+	},	
 	
-	
+	["Test Man's Brother"] = {
+
+		["Shop Name"] = "Test Man's Brother's Shop",
+		["Shop Type"] = "Resources",
+
+		["ShopTheme"] = {
+			["Shop Banner"] = "rbxassetid://7024895384", --Pickaxe Shop Banner
+			["Primary Color"] = {R=179,G=179,B=179},
+			["Secondary Color"] = {R=106,G=106,B=106}
+
+		},
+
+		["Voice"] = {
+			--Do not include angry voice, kids playing may get scared
+
+			--["Happy"] = #######,
+			--["Sad"] = ######,
+			["Neutral"] = 6467449877
+		},
+
+		["Face"] = {
+			--["Happy"] = ImageId,
+			--["Sad"] = ImageId,
+			--["Neutral"] = ImageId			
+		},
+
+		["Dialogue"] = { --Does [index] need to be present?
+			["Starters"] = {"Hey! I'm Test Man's Brother!", "Hi PLAYER!", "Need any blocks today?"},
+			["Goodbyes"] = {"Goodbye!", "See ya PLAYER"},
+			["Cannot Afford Item"] = {"Sorry, you need MISSINGFUNDS more to purchase the ITEM"},
+			["Item Already Purchased"] = {"You already have the ITEM!"},
+			["Thank You For Purchase"] = {"Thank you for your purchase!"},
+			["Dialogue1"] = {"Hi There PLAYER! How are you?", "I'm Test Man, welcome to my shop!", "I set up shop not too long ago"}
+
+
+			--Player responses with shop keeper? (label as conversations?)
+		},
+
+		["Items"] = { --Sort by type (Rarity = ItemType)
+			{RS.InventoryItems.Mining.Stone, 2, "Coins"},
+			{RS.InventoryItems.Foraging["Glowing Mushroom"], 50, "Coins"},
+			{RS.InventoryItems.Mining.Coal, 10, "Coins"},
+			{RS.InventoryItems.Mining.Obsidian, 500, "Coins"}
+			
+		}	
+	}
 	
 	
 }
@@ -60,3 +105,4 @@ local NPCData = {
 --to go through each element of this table to get item info to be displayed
 
 return NPCData
+
