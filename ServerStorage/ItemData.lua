74a7281e85fe2a06b80186e657e15178b491bc13
Ben Paulson <73416124+Benja-Pauls@users.noreply.github.com>
2022-01-03 10:31:39 -0600

    Update and rename ItemData.lua to ServerStorage/ItemData.lua

--- ServerStorage/ItemData.lua
+++ ServerStorage/ItemData.lua
@@ -1,3 +1,6 @@
+--(ModuleScript)
+--Script that may be used to replace the physical values attached to different items. However, this is not required.
+
 local RS = game.ReplicatedStorage
 
 local ItemData = {
